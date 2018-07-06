1226c4b1742890d8f6f711f5ad113e33a60195a3
Giovanni Zenari <giovannizenari@Giovannis-MacBook-Pro.local>
2018-07-06 21:01:32 +0200

    Project restructure
    Chang ein the project structure to pass from a model structure to a package structure to manage the import safer than before

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired,Length, Email, EqualTo
+from wtforms.validators import DataRequired,Length, Email, EqualTo, ValidationError
+from models import User, Post
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(), Length(min=2,max=20)])
@@ -9,6 +10,17 @@ class RegistrationForm(FlaskForm):
     confirmed_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
     submit = SubmitField('Sign Up')
+
+    def validate_username(self,username):
+        print username.data
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken please choose another one')
+
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken please choose another one')
 
 
 class LoginForm(FlaskForm):
